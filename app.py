ce8ddf4bb093fbfa428e22f34fa40478a247c7eb
Dhanika Botejue <dhanikabotejue@gmail.com>
2025-07-17 14:20:45 -0400

    fixed empty caption bug

--- app.py
+++ app.py
@@ -33,6 +33,10 @@ def get_video():
         duration = round((float(segment["end"]) - start), 2)
         words = segment["text"].strip()
 
+        # Do not create empty caption when person not talking for long time
+        if words == "" or duration == 0.0:
+            continue
+
         info = [words, duration]
         transcript_details[start] = info
         #print(words, timing)
@@ -56,7 +60,6 @@ def translate(text, target_lang='es', source_lang='en'):
 
 transcript_details = {}
 get_video()
-
 # Must put correct 'language code' e.g. Spanish is es and English is en
 source = input("Language to translate from: ")
 target = input("Language to translate to: ")
@@ -64,7 +67,7 @@ target = input("Language to translate to: ")
 print("Translating...")
 
 for key in transcript_details:
-    transcript_details[key][0] = translate(transcript_details[key][0], target, source)
+    transcript_details[key].append(translate(transcript_details[key][0], target, source))
 
 print(transcript_details)
 
